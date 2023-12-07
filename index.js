ea2f62e1f3519dff42590c18bf96170eb1d7995e
Sahil Kumar <reavensnape07@gmail.com>
2023-12-07 17:01:51 +0530

    index.js updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: 'https://product-cart-frontend.vercel.app', // Replace with your frontend app URL
+  origin: '*', 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
